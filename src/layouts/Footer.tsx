6b858ce6fb4bb2b2419c37ff84fdab5a6ac94f84
Yureusu <madlogicstuiox@gmail.com>
2025-12-12 01:20:26 +0800

    Fixed footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -39,20 +39,19 @@ function Footer({ isDark }: FooterProps) {
 
                 <div className={`${isMobile? "flex-col" : "flex-row"}
                     flex-1 h-full w-full flex items-center justify-start pt-[calc(0.4vw+0.6rem)]`}>
-                    <div className={`${isMobile? "flex-col-reverse" : "flex-col"}
+                    <div className={`${isMobile? "flex-col" : "flex-col"}
                         flex-1 h-full w-full gap-[calc(0.4vw+0.6rem)] p-[calc(0.4vw+0.6rem)] flex items-start justify-start`}>
                         <span className={`${isDark? "text-[var(--light-color)]" : "text-[var(--dark-color)]"} 
-                            ${isMobile? "w-full text-center" : ""}
-                            spacegrotesk text-[calc(0.6vw+0.9rem)] font-semibold`}>Follow our socials</span>
+                            ${isMobile? "w-full text-center text-[calc(0.6vw+1rem)]" : "text-[calc(0.6vw+0.8rem)]"}
+                            spacegrotesk font-semibold`}>Follow our socials</span>
                         <div className={`${isMobile? "w-full" : ""}
                             flex flex-row items-center justify-center gap-[calc(0.4vw+0.6rem)]`}>
-                            <i className='bxl bx-facebook bx-tada-hover text-[calc(0.8vw+1.4rem)] cursor-pointer primary'></i>
-                            <i className='bxl bx-github bx-tada-hover text-[calc(0.8vw+1.4rem)] cursor-pointer primary'></i> 
-                            <i className='bxl bx-instagram bx-tada-hover text-[calc(0.8vw+1.4rem)] cursor-pointer primary'></i>
-                            <i className='bxl bx-twitter-x bx-tada-hover text-[calc(0.8vw+1.4rem)] cursor-pointer primary'></i>
+                            <i className='bxl bx-facebook bx-tada-hover text-[calc(0.8vw+1.6rem)] cursor-pointer primary'></i>
+                            <i className='bxl bx-github bx-tada-hover text-[calc(0.8vw+1.6rem)] cursor-pointer primary'></i> 
+                            <i className='bxl bx-instagram bx-tada-hover text-[calc(0.8vw+1.6rem)] cursor-pointer primary'></i>
+                            <i className='bxl bx-twitter-x bx-tada-hover text-[calc(0.8vw+1.6rem)] cursor-pointer primary'></i>
                         </div>
-                        <span className={`${isMobile? "w-full text-justify" : ""}
-                            text-[calc(0.4vw+0.8rem)]`}>
+                        <span className={`${isMobile? "w-full text-justify text-[calc(0.6vw+0.8rem)]" : "text-[calc(0.8vw+0.4rem)]"}`}>
                             Mad Logic Studio is a modern web and app development company turning bold ideas into exceptional digital products. 
                             We combine smart strategy, creative problem-solving, and cutting-edge technology to build fast, scalable, and beautifully designed solutions. 
                             Our team values clean code, user-focused design, and a touch of “mad” innovation in everything we do. 
@@ -63,8 +62,8 @@ function Footer({ isDark }: FooterProps) {
                     <div className="flex-1 h-full w-full gap-[calc(0.4vw+0.6rem)] p-[calc(0.4vw+0.6rem)] flex flex-col items-start justify-between">
                         <div className="flex flex-col items-start justify-start gap-[calc(0.4vw+0.6rem)] text-[calc(0.4vw+0.7rem)]">
                             <span className={`${isDark? "text-[var(--light-color)]" : "text-[var(--dark-color)]"} 
-                                ${isMobile? "w-full text-center" : ""}
-                                spacegrotesk text-[calc(0.6vw+0.9rem)] font-semibold`}>Subscribe to our newsletter</span>
+                                ${isMobile? "w-full text-center text-[calc(0.6vw+1rem)]" : "text-[calc(0.6vw+0.8rem)]"}
+                                spacegrotesk font-semibold`}>Subscribe to our newsletter</span>
                             <div className="relative h-auto w-full flex flex-start items-start justify-start">
                                 <input type="email" placeholder="What's your email?" 
                                     className={`${isDark? "" : "border border-[var(--border-color)]"}
@@ -74,15 +73,17 @@ function Footer({ isDark }: FooterProps) {
                                 py-[calc(0.3vw+0.4rem)] bg-[var(--primary-color)] text-[var(--light-color)]
                                 hover:bg-[var(--secondary-color)] transition duration-300 ease-in-out">Subscribe</span>
                             </div>
-                            <div className="w-auto flex flex-row items-center justify-start gap-[calc(0.1vw+0.2rem)] text-[calc(0.4vw+0.8rem)] flex-wrap">
+                            <div className={`${isMobile? "text-[calc(0.6vw+0.8rem)]" : "text-[calc(0.8vw+0.4rem)]"}
+                                w-auto flex flex-row items-center justify-start gap-[calc(0.1vw+0.2rem)] flex-wrap`}>
                                 <span>By entering your email, you agree to our</span>
                                 <u className="cursor-pointer primary">Terms & Conditions </u>
                                 <span>and</span>
                                 <u className="cursor-pointer primary">Privacy Policy.</u>
                             </div>
                         </div>
-                        <div className="w-full flex flex-row items-center justify-between">
-                            <span className="cursor-pointer primary text-[calc(0.4vw+0.8rem)]">© Mad Logic Studio. All rights reserved.</span>
+                        <div className={`${isMobile? "text-[calc(0.6vw+0.8rem)]" : "text-[calc(0.8vw+0.4rem)]"}
+                            w-full flex flex-row items-center justify-between`}>
+                            <span className="cursor-pointer primary">© Mad Logic Studio. All rights reserved.</span>
                             <i className='bx bx-chevron-up-square bx-spin-hover text-[calc(1vw+1.6rem)] cursor-pointer primary'
                                 onClick={scrollToTop}></i> 
                         </div>
